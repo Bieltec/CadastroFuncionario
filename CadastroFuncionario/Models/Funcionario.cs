d17ef4e8a8dfa7bab025352390fd38b754fa1944
Gabriel Henrique <ghds96@hotmail.com>
2022-06-30 14:05:51 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroFuncionario/Models/Funcionario.cs
@@ -0,0 +1,14 @@
+﻿namespace CadastroFuncionario.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Contato { get; set; }
+        public string Genero { get; set; }
+        // passar propriedade com datetype, aprender. 
+        public string DataNascimento { get; set; }
+        public string Email { get; set; }
+      
+    }
+}
